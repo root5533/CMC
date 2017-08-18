69dc95d042c03fb5fb8df17a1a01d2e441319104
IshiPerera <31043314+IshiPerera@users.noreply.github.com>
2017-08-18 11:05:28 +0530

    latest one

---
+++ Thilini/index.php
@@ -0,0 +1,16 @@
+<html>
+
+    <head>
+        <title>Index Page</title>
+    </head>
+
+    <body>
+        <form>
+            <
+
+        </form>
+
+
+    </body>
+
+</html>
---
+++ Thilini/login.php
@@ -0,0 +1,35 @@
+
+<html>
+
+    <head>
+        <title>Log In</title>
+
+    </head>
+
+    <body>
+
+        <form action="loginaction.php" method="POST">
+
+            <table>
+
+                <tr>
+                    <td><label>User Name:</label></td>
+                    <td><input type="text" name="username"></td>
+                </tr>
+
+                <tr>
+                    <td><label>Password:</label></td>
+                    <td><input type="password" name="password"></td>
+                </tr>
+
+                <tr>
+                    <td><input type="submit" name="login" value="Log In"></td>
+                </tr>
+            </table>
+
+        </form>
+
+
+    </body>
+
+</html>
