e231e27d51bc5d01d7f375659bbebc75408204c7
IshiPerera <31043314+IshiPerera@users.noreply.github.com>
2017-08-18 20:38:03 +0530

    up to date

---
+++ Thilini/jobentry.php
@@ -0,0 +1,66 @@
+<html>
+
+    <head>
+        <title>Job Entry Page</title>
+    </head>
+
+    <body>
+
+        <div>
+            <form>
+                <table>
+                    <tr>
+                        <td><label>Job Number</label></td>
+                        <td><input type="text" name="jobnumber"></td>
+
+                    </tr>
+
+                    <tr>
+                        <td><label>Department</label></td>
+                        <td><input type="text" name="department"></td>
+                    </tr>
+
+                    <tr>
+                        <td><label>Engineer Code</label></td>
+                        <td><input type="text" name="eng"></td>
+                    </tr>
+
+                    <tr>
+                        <td><label>Applicant</label></td>
+                        <td><input type="text" name="applicant"></td>
+                    </tr>
+
+                    <tr>
+                        <td>Vehicle No</td>
+                        <td><input type="text" name="vehicle"></td>
+                    </tr>
+
+                    <tr>
+                        <td><label>Item</label></td>
+                        <td><input type="text" name="item"></td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="2">
+                    <input type="submit" value="Create">
+                    <input type="submit" value="Update">
+                    <input type="submit" value="Delete">
+                    <input type="reset" value="Cancel">
+                        </td>
+                    </tr>
+
+
+                </table>
+
+
+            </form>
+
+
+
+        </div>
+
+
+
+    </body>
+
+</html>
